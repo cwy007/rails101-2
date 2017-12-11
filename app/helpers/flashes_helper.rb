b3a1e4babc3b57d2ffdbd006995a2166badeb96a
cwy007 <chanweiyan007@gmail.com>
2017-12-12 00:14:21 +0800

    generate parking model

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -2,10 +2,10 @@ module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning" }.freeze
 
   def flash_class(key)
-    FLASH_CLASSES.fetch key.to_sym, key
+    FLASH_CLASSES.fetch key.to_sym, key   # 用fetch 方法的第二个参数设定默认值
   end
 
   def user_facing_flashes
-    flash.to_hash.slice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "warning"  # slice 取出这些键   并返回
   end
 end
